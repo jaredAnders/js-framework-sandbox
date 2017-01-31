0587ce44238afb81efb8c6ab56e74a3d802d0ab6
jaredandersgilbert@gmail.com <jaredandersgilbert@gmail.com>
2017-01-31 10:08:40 -0500

    adds initial js and html index files

---
+++ jupiter/app/index.js
@@ -0,0 +1,10 @@
+import React, { Componenent } from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  return (
+    <div>Hello World</div>
+  )
+};
+
+ReactDOM.render(<App />, document.getElementById('app'));
