e28b70fe99565a1dcec87178641a75eff0bc781b
jaredandersgilbert@gmail.com <jaredandersgilbert@gmail.com>
2017-02-08 21:31:03 -0500

    adds functions for checking and removing tasks

--- meteor-react/imports/ui/Task.jsx
+++ meteor-react/imports/ui/Task.jsx
@@ -1,9 +1,34 @@
 import React, { Component, PropTypes } from 'react';
 
+import { Tasks } from '../api/tasks.js';
+
 export default class Task extends Component {
+  toggleChecked() {
+    Tasks.update(this.props.task._id, {
+      $set: { checked: !this.props.task.checked },
+    });
+  }
+
+  deleteTask() {
+    Tasks.remove(this.props.task._id);
+  }
+
   render() {
+    const taskClassName = this.props.task.checked ? 'collection-item checked' : 'collection-item';
+
     return (
-      <li className="collection-item">{this.props.task.text}</li>
+      <li className={taskClassName}>
+        <input
+          type="checkbox"
+          checked={this.props.task.checked}
+          onClick={this.toggleChecked.bind(this)}
+          id={this.props.task._id}
+        />
+
+        <label htmlFor={this.props.task._id}>{this.props.task.text}</label>
+
+        <i className="material-icons delete right" onClick={this.deleteTask.bind(this)}>remove_circle_outline</i>
+      </li>
     );
   }
 }
