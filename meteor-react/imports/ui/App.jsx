006e22ee61f0288d8ab4f20b4df5d812bd6537ea
jaredandersgilbert@gmail.com <jaredandersgilbert@gmail.com>
2017-02-11 15:33:55 -0500

    adds filter to show/hide completed tasls

--- meteor-react/imports/ui/App.jsx
+++ meteor-react/imports/ui/App.jsx
@@ -7,6 +7,14 @@ import { Tasks } from '../api/tasks.js';
 import Task from './Task.jsx';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      hideCompleted: false,
+    };
+  }
+
   handleSubmit(event) {
     event.preventDefault();
 
@@ -20,8 +28,18 @@ class App extends Component {
     ReactDOM.findDOMNode(this.refs.textInput).value = '';
   }
 
+  toggleHideCompleted() {
+    this.setState({
+      hideCompleted: !this.state.hideCompleted,
+    });
+  }
+
   renderTasks() {
-    return this.props.tasks.map((task) => (
+    let filteredTasks = this.props.tasks;
+    if (this.state.hideCompleted) {
+      filteredTasks = filteredTasks.filter(task => !task.checked);
+    }
+    return filteredTasks.map((task) => (
       <Task key={task._id} task={task} />
     ));
   }
@@ -31,13 +49,25 @@ class App extends Component {
       <div className="container">
         <header>
           <h1>Todo List</h1>
-            <form className="new-task" onSubmit={this.handleSubmit.bind(this)} >
-              <input
-                type="text"
-                ref="textInput"
-                placeholder="add a task"
-                />
-            </form>
+
+
+          <input
+            type="checkbox"
+            readOnly
+            checked={this.state.hideCompleted}
+            onClick={this.toggleHideCompleted.bind(this)}
+            id="toggle-completed"
+          />
+
+        <label htmlFor="toggle-completed" className="hide-completed">Hide Completed Tasks</label>
+
+          <form className="new-task" onSubmit={this.handleSubmit.bind(this)} >
+            <input
+              type="text"
+              ref="textInput"
+              placeholder="add a task"
+              />
+          </form>
         </header>
 
         <ul className="collection">
