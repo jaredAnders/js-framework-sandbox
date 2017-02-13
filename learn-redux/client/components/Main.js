519795db91900393080cc6d96802d38d71870707
jaredandersgilbert@gmail.com <jaredandersgilbert@gmail.com>
2017-02-13 16:34:17 -0500

    builds and imports main component

---
+++ learn-redux/client/components/Main.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router';
+
+export default Main = React.createClass({
+  render() {
+    return (
+      <div>
+        <h1>
+          <Link to="/">Instaface</Link>
+        </h1>
+      </div>
+    )
+  }
+});
