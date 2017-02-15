ae1e8561fd3b87217cc0a18d225b38e2e8a723b7
jaredandersgilbert@gmail.com <jaredandersgilbert@gmail.com>
2017-02-15 12:48:06 -0500

    adds action creators

---
+++ learn-redux/client/actions/actionCreators.js
@@ -0,0 +1,21 @@
+export function increment(index) {
+  return {
+    type: 'INCREMENT_LIKES',
+    index
+  }
+}
+
+export function addComment(postId, author, comment) {
+  return {
+    type: 'ADD_COMMENT',
+    postId,
+    author,
+    comment
+  }
+}
+
+export function removeComment(postId, index) {
+  type: 'REMOVE_COMMENT',
+  postId,
+  index
+}
